d7eaeb32659bdc76d6b0b1710a85e22f05a0d2d5
Lachezar Kutsarov <lkutsarov@gmail.com>
2018-09-20 21:37:42 +0200

    Create SolutionIntegersWithMultipleOccurrences.java

---
+++ SolutionIntegersWithMultipleOccurrences.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Solves the problem as per the original conditions of the HackerRank
+ * challenge, where only unique integers are allowed as an input.
+ */
+public class StreamlinedUniqueIntegers {
+
+	public static int pairs(int targetValue, int[] inputArray) {
+
+		Arrays.sort(inputArray);
+		int indexOfSmallerInteger = 0;
+		int indexOfLargerInteger = 1;
+		int totalPairs = 0;
+
+		while (indexOfLargerInteger < inputArray.length) {
+
+			while (indexOfLargerInteger < inputArray.length
+					&& inputArray[indexOfLargerInteger] - inputArray[indexOfSmallerInteger] < targetValue) {
+				indexOfLargerInteger++;
+			}
+
+			if (indexOfLargerInteger < inputArray.length
+					&& inputArray[indexOfLargerInteger] - inputArray[indexOfSmallerInteger] == targetValue) {
+				totalPairs++;
+				indexOfLargerInteger++;
+			}
+
+			indexOfSmallerInteger++;
+		}
+		return totalPairs;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner reader = new Scanner(System.in);
+		int sizeOfArray = reader.nextInt();
+		int targetValue = reader.nextInt();
+		int[] inputArray = new int[sizeOfArray];
+
+		for (int i = 0; i < sizeOfArray; i++) {
+			inputArray[i] = reader.nextInt();
+		}
+		reader.close();
+
+		int result = pairs(targetValue, inputArray);
+		System.out.println(result);
+	}
+}
